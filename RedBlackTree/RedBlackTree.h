56748a9fa13165e721261851e31703f4ae8686a5
Boyoon Choi <chlqhdbs11@naver.com>
2021-12-16 22:16:09 +0100

    Add files via upload

---
+++ RedBlackTree/RedBlackTree.h
@@ -0,0 +1,36 @@
+#ifndef RedBlackTree_h
+#define RedBlackTree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagRBTNode{
+    struct tagRBTNode* Parent;
+    struct tagRBTNode* Right;
+    struct tagRBTNode* Left;
+    
+    enum { RED, BLACK } Color;
+    
+    ElementType Data;
+} RBTNode;
+
+void RBT_DestroyTree(RBTNode* Tree);
+
+RBTNode* RBT_CreateNode(ElementType NewData);
+void RBT_DestroyNode(RBTNode* Node);
+
+RBTNode* RBT_SearchNode(RBTNode* Tree, ElementType Target);
+RBTNode* RBT_SearchMinNode(RBTNode* Tree);
+void RBT_InsertNode(RBTNode** Tree, RBTNode* NewNode);
+void RBT_InsertNodeHelper(RBTNode** Tree, RBTNode* NewNode);
+RBTNode* RBT_RemoveNode(RBTNode** Tree, ElementType Target);
+void RBT_RebuildAfterInsert(RBTNode** Tree, RBTNode* NewNode);
+void RBT_RebuildAfterRemove(RBTNode** Root, RBTNode* X);
+
+void RBT_PrintTree(RBTNode* Node, int Depth, int BlackCount);
+void RBT_RotateLeft(RBTNode** Root, RBTNode* Parent);
+void RBT_RotateRight(RBTNode** Root, RBTNode* Parent);
+
+#endif /* RedBlackTree_h */
